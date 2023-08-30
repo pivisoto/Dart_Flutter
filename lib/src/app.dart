c7ed01c7695d0dab5a47721d6eb136838bc2dd77
Pivisoto <pivisoto10@gmail.com>
2023-08-30 12:55:21 -0300

    Flutter : contador imagem

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget{
+class AppState extends State<App>{
+  int numeroimagens = 0;
+  @override
   Widget build(BuildContext context){
     return MaterialApp(
     home: Scaffold(
+      body:Center(child:Text("O número de vezes clicados foi $numeroimagens")),
       appBar: AppBar(
         title : const Text("Minhas Imagens")
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => print('Estou no arquivo app.dart'),
+        onPressed:()=>setState((){
+        numeroimagens++;
+        }),
       ),
     ),
   );
   }
+}
+class App extends StatefulWidget{
+  State<App> createState(){
+    return AppState();
+  } 
 }
